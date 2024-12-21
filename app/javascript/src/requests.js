79af33f3c0081f120329663226a4216296a72b19
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-12-21 13:54:19 +0000

    add get & post ajax requests

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -1,1 +1,35 @@
-console.log('ajax requests');
+import $ from 'jquery';
+
+// CSRF ajax setup for other requests
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
+// ajax requests for exporting to index.js
+export var indexTasks = function (successCB, errorCB) {
+  var request = {
+    type: 'GET',
+    url: 'api/tasks?api_key=1',
+    success: successCB,
+    error: errorCB
+  }
+  $.ajax(request);
+};
+
+export var postTask = function (content, successCB, errorCB) {
+  var request = {
+    type: 'POST',
+    url: 'api/tasks?api_key=1',
+    data: {
+      task: {
+        content: content
+      }
+    },
+    success: successCB,
+    error: errorCB
+  }
+  $.ajax(request);
+};
+
